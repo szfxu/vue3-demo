f0fef60876a7a635ae7a656204c32ce71106600b
szfxu <946918996@qq.com>
2023-02-07 18:42:58 +0800

    修改

---
+++ .history/src/components/11-app_20230206181549.vue
@@ -0,0 +1,16 @@
+<template>
+     <!-- 父组件 -->
+     <Child :num="num" @fn="changeNum"></Child>
+</template>
+
+<script lang='ts' setup>
+import Child from "./11-appChild.vue"
+import { ref } from 'vue'
+let num = ref(20)
+const changeNum = ()=>{
+     num.value++
+}
+</script>
+<style lang = "less" scoped>
+    
+</style>
